f603456096e0d65bf0eb6d5973f3aa73dbaff887
KyungSeok <leeseok8347@gmail.com>
2018-03-04 02:17:17 +0900

    [Bash Shell Script] 변수 작성

---
+++ BashShellScript/start.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+# 문장 처음에 /bin/bash라고 주석을 붙여 bashshell로 작성된 코드임을 나타낸다.
+
+# 터미널에 hello world를 echo 커맨드로 출력
+echo "hello world"
+
+# ; 를 사용해서 연이은 실행이 가능
+echo "hello world" ; echo "hello world1"
+
+# exit 0 정상 종료되면0, 비정상 종료이면 0이 아닌 값
+exit 0
